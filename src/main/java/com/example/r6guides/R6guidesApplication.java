f69e2612fd081b4efbb6aaa7c3d2a8ed88b6c87e
nilsson46 <nilssonsimon46@gmail.com>
2025-02-26 16:20:08 +0100

    Initial commit

---
+++ src/main/java/com/example/r6guides/R6guidesApplication.java
@@ -0,0 +1,13 @@
+package com.example.r6guides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R6guidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R6guidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/r6guides/R6guidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.r6guides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R6guidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
