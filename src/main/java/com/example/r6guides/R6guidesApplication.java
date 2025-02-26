97dbadc645bebdfcd2e460a49e0bcca285998fa2
nilsson46 <nilssonsimon46@gmail.com>
2025-02-26 19:34:05 +0100

    Trying github actions

--- src/main/java/com/example/r6guides/R6guidesApplication.java
+++ src/main/java/com/example/r6guides/R6guidesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class R6guidesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(R6guidesApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
