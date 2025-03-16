a012efc0b7d5cd28476df9a0188059110724b4b1
nilsson46 <nilssonsimon46@gmail.com>
2025-03-16 20:09:56 +0100

    Basic model to start with

---
+++ src/main/java/com/example/r6guides/models/Map.java
@@ -0,0 +1,20 @@
+package com.example.r6guides.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Map {
+
+    private Long id;
+    private String name;
+    private String imageUrl;
+    private String description;
+    private String objective;
+    private String spawn;
+    private String cameras;
+    private String destructibleWalls;
+    private String destructibleFloors;
+    private String destructibleCeilings;
+    private String mapGuide;
+    //Image url?
+}
