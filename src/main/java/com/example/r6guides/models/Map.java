3592a9ddedbfa242d6e72f5fb1dc854ef05d9ac2
nilsson46 <nilssonsimon46@gmail.com>
2025-03-16 20:12:46 +0100

    Added id and columns

--- src/main/java/com/example/r6guides/models/Map.java
+++ src/main/java/com/example/r6guides/models/Map.java
@@ -1,13 +1,19 @@
 package com.example.r6guides.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Map {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @Column(nullable = false)
     private String imageUrl;
+    @Column
     private String description;
    // private String objective;
    // private String spawn;
