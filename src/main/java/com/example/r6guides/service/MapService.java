87c0013ab70423f2835469958c099d664b6714d4
nilsson46 <nilssonsimon46@gmail.com>
2025-03-17 10:49:09 +0100

    Map service done

---
+++ src/main/java/com/example/r6guides/service/MapService.java
@@ -0,0 +1,47 @@
+package com.example.r6guides.service;
+
+import com.example.r6guides.models.Map;
+import com.example.r6guides.repository.MapRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MapService {
+    private final MapRepository mapRepository;
+
+    @Autowired
+    public MapService(MapRepository mapRepository) {
+        this.mapRepository = mapRepository;
+    }
+
+    public List<Map> getAllMaps() {
+        return mapRepository.findAll();
+    }
+
+    public Map getMapById(long id) {
+        return mapRepository.findById(id);
+    }
+
+    public Map getMapByName(String name) {
+        return mapRepository.findByName(name);
+    }
+
+    public List<Map> getMapsByName(String name) {
+        return mapRepository.findAllByNameContaining(name);
+    }
+
+    public Map addMap(Map map) {
+        return mapRepository.save(map);
+    }
+
+    public void deleteMap(long id) {
+        mapRepository.deleteById(id);
+    }
+
+    public Map updateMap(Map map) {
+        return mapRepository.save(map);
+    }
+
+}
