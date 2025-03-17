dddf75a59517253ba8f0f55dc2339c480800b994
nilsson46 <nilssonsimon46@gmail.com>
2025-03-17 11:18:38 +0100

    Floor repo done

---
+++ src/main/java/com/example/r6guides/repository/FloorRepository.java
@@ -0,0 +1,14 @@
+package com.example.r6guides.repository;
+
+import com.example.r6guides.models.Floor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FloorRepository extends JpaRepository<Floor, Long> {
+    List<Floor> findByMapId(Long mapId);
+    List<Floor> findByMapIdOrderByLevel(Long mapId);
+    //List<Floor> findByInRankedPool(boolean inRankedPool);
+}
