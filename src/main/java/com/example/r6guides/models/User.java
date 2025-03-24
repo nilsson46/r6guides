36ad19abc0b230927070d91c8c546cdc5a4004f4
nilsson46 <nilssonsimon46@gmail.com>
2025-03-24 15:59:16 +0100

    Model user

---
+++ src/main/java/com/example/r6guides/models/User.java
@@ -0,0 +1,28 @@
+package com.example.r6guides.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+
+
+}
