3aa06eaf335cd1d5de00d1d1ecbab51fa48f3b15
nilsson46 <nilssonsimon46@gmail.com>
2025-03-27 09:45:30 +0100

    Added dto for login and register

---
+++ src/main/java/com/example/r6guides/DTO/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.example.r6guides.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/r6guides/DTO/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.example.r6guides.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String username;
+    private String email;
+    private String token;
+    private String role;
+}
