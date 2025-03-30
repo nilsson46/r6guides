e82fe6982881cabb146d5003a30c9bae6d9888cd
nilsson46 <nilssonsimon46@gmail.com>
2025-03-30 16:39:07 +0200

    JWT util created and added in to the UserService

--- src/main/java/com/example/r6guides/service/UserService.java
+++ src/main/java/com/example/r6guides/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.r6guides.DTO.LoginRequest;
 import com.example.r6guides.DTO.LoginResponse;
 import com.example.r6guides.models.User;
 import com.example.r6guides.repository.UserRepository;
+import com.example.r6guides.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,14 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     public LoginResponse login(LoginRequest loginRequest) {
         User user = userRepository.findByUsername(loginRequest.getUsername()).orElseThrow(() -> new RuntimeException("User not found"));
 
         if (passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-            String token = "generated-jwt-token";
+            String token = jwtUtil.generateToken(user.getUsername());
             return new LoginResponse(user.getUsername(), user.getEmail(), token, user.getRoleType());
         } else {
             throw new RuntimeException("Invalid credentials");
