30913ed56df8b7ef7ff3a407aa1b6d203c2174bb
nilsson46 <nilssonsimon46@gmail.com>
2025-12-02 22:25:21 +0100

    Cleaning ideas

---
+++ src/main/java/com/example/r6guides/controller/LineController.java
@@ -0,0 +1,4 @@
+package com.example.r6guides.controller;
+//TODO: implement line controller with endpoints for line service and models and take the code from Mapcontroller
+public class LineController {
+}
